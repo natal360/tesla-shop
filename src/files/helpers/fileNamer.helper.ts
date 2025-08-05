1c43577333596232f163a3fcd8591b48ef88f01a
natal360 <drossel.fb@gmail.com>
2025-08-05 12:55:59 +0900

    file upload

---
+++ src/files/helpers/fileNamer.helper.ts
@@ -0,0 +1,17 @@
+import { v4 as uuid } from "uuid";
+
+export const fileNamer = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  callback: Function,
+) => {
+  // console.log(file)
+  if (!file) return callback(new Error('File is empty'), false);
+
+  const fileExtenstion = file.mimetype.split('/')[1];
+
+  const fileName = `${uuid()}.${fileExtenstion}`
+
+  return callback(null, fileName);
+  
+};
